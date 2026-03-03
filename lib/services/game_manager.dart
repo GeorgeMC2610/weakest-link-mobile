220bac60660c5249d4d592edbc7a49ea4eba8652
GeorgeMC2610 <giorgosseimenis@gmail.com>
2026-03-03 11:18:43 +0200

    game manager initialization

--- lib/services/game_manager.dart
+++ lib/services/game_manager.dart
@@ -1,3 +1,31 @@
-class GameManager {
-  
+import 'package:flutter/material.dart';
+import 'package:weakest_link/classes/player.dart';
+
+class GameManager with ChangeNotifier {
+  GameManager._internal();
+  static final GameManager _instance = GameManager._internal();
+  factory GameManager() => _instance;
+
+  List<Player> _players = [];
+  List<Player> get players => _players;
+
+  void setPlayers(List<Player> players) {
+    _players = players;
+    notifyListeners();
+  }
+
+  int _roundNumber = 1;
+  int get roundNumber => _roundNumber;
+
+  void incrementRoundNumber() {
+    _roundNumber++;
+    notifyListeners();
+  }
+}
+
+enum GameState {
+  notStarted,
+  menu,
+  playing,
+  voting,
 }
