73666e3ae813d5b4ca318abe7e7524de2c7eb57c
GeorgeMC2610 <giorgosseimenis@gmail.com>
2026-03-03 11:31:14 +0200

    player extra fields

--- lib/classes/player.dart
+++ lib/classes/player.dart
@@ -6,9 +6,14 @@ class Player {
   bool isEliminated;
   bool isWeakestLink;
   bool isStrongestLink;
+  int votes;
+  int pointsSaved;
+  int wrongAnswers;
+  int rightAnswers;
 
   Player({
     required this.name, required this.color, this.isEliminated = false,
-    this.isStrongestLink = false, this.isWeakestLink = false
+    this.isStrongestLink = false, this.isWeakestLink = false, this.votes = 0,
+    this.pointsSaved = 0, this.wrongAnswers = 0, this.rightAnswers = 0,
   });
 }
